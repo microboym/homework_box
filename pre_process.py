f8431e827cad8045ac72c0521c4d55c0a130a8e6
Tony Mai <microboym@outlook.com>
2021-05-20 21:43:38 +0800

    Revert "Update pre_process.py"
    This reverts commit dc48139adc7f20cdba8ded2d3e7bf0d1bfbf09fc.

--- pre_process.py
+++ pre_process.py
@@ -45,25 +45,25 @@ def extract_peek(array_vals, min_vals=10, min_rect=5):
 def find_borders(img, length, min_size=0, max_size=float("inf")):
     # Extract the row
     hori_vals = np.sum(img, axis=1)
-    hori_points = extract_peek(hori_vals, min_vals=20000, min_rect=100)
-    for hori_point in hori_points:
-        extract_img = img[hori_point[0]:hori_point[1], :]
+    hori_points = extract_peek(hori_vals)
+    hori_point = hori_points[0]
+    extract_img = img[hori_point[0]:hori_point[1], :]
 
-        vec_vals = np.sum(extract_img, axis=0)
-        vec_points = extract_peek(vec_vals, min_rect=0)
-        borders = []
+    vec_vals = np.sum(extract_img, axis=0)
+    vec_points = extract_peek(vec_vals, min_rect=0)
+    borders = []
 
-        size_of_border = lambda x: (x[1][1]-x[0][1]) * (x[1][0]-x[0][0])
-        for vect_point in vec_points:
-            border = [(vect_point[0], hori_point[0]), (vect_point[1], hori_point[1])]
-            if size_of_border(border) >= min_size and size_of_border(border) <= max_size:
-                borders.append(border)
+    size_of_border = lambda x: (x[1][1]-x[0][1]) * (x[1][0]-x[0][0])
+    for vect_point in vec_points:
+        border = [(vect_point[0], hori_point[0]), (vect_point[1], hori_point[1])]
+        if size_of_border(border) >= min_size and size_of_border(border) <= max_size:
+            borders.append(border)
 
-        if len(borders) > length:
-            borders.sort(key=size_of_border)
-            borders = borders[:length]
-        if len(borders) == length:
-            return sorted(borders, key=lambda x: x[0][0])
+    if len(borders) > length:
+        borders.sort(key=size_of_border)
+        borders = borders[:length]
+    if len(borders) == length:
+        return sorted(borders, key=lambda x: x[0][0])
 
 def extract_numbers(bin, borders, size=(28, 28)):
     img_data = np.zeros((len(borders), size[0], size[0], 1), dtype='uint8')
@@ -81,15 +81,15 @@ def draw_border(img, borders):
         cv2.rectangle(img, border[0], border[1], (0, 0, 255))
     return img
 
-def process_image(image, length=5, min_size=0, max_size=float("inf"), roi=(0, 0, 0, 0)):
+def process_image(image, length=5, roi=(0, 0, 0, 0)):
     binary = access_binary(image, roi=roi)
-    borders = find_borders(binary, length=length, min_size=min_size, max_size=max_size)
+    borders = find_borders(binary, length=length)
     data = extract_numbers(binary, borders)
     return data
 
-def trace_image(image, length=5, min_size=0, max_size=float("inf"), roi=(0, 0, 0, 0)):
+def trace_image(image, length=5, roi=(0, 0, 0, 0)):
     binary = access_binary(image, roi=roi)
-    borders = find_borders(binary, length=length, min_size=min_size, max_size=max_size)
+    borders = find_borders(binary, length=length)
     with_boders = draw_border(image.copy(), borders)
     data = extract_numbers(binary, borders)
     return binary, with_boders, data
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     path = input("Input path: ")
     image = cv2.imread(path)
 
-    #
+    # Trace
     binary, with_boders, data = trace_image(image)
 
     plt.subplot(2, 2, 1), plt.imshow(binary, "gray"), plt.title("Binary")
